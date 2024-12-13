ecdf84efd7c54238ecef1de2b00d99b050e71994
Mahesh S Biradar <maheshsb6677@gmail.com>
2024-12-13 10:11:39 +0530

    Adding existing files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>an ecmmerce website is an online platform that facilitates buying and selling of products
+                or service over the internet serves as a virtual market place where businesses and
+                individual showcase their products, interact with customers and conduct
+                transactions without the need for a physical presence, E-commerce webistes have
+                gained immense popularity due to their convienience accessibility,
+                and the global reach trhey offer
+            </p>
+            <p>
+               interact with customers and conduct
+                transactions without the need for a physical presence, E-commerce webistes have
+                gained immense popularity due to their convienience accessibility,
+                and the global reach trhey offer
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
